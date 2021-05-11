18674d9d7a5e271b40718a036d7121028790e805
Valentina Antollini <antolliniv@hotmail.com>
2021-05-11 11:48:02 +0100

    added fonts

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Landing() {
-    return (
-        <div>
-            landing
-        </div>
-    )
+  return (
+    <div>
+      <h1> landing</h1>
+    </div>
+  );
 }
