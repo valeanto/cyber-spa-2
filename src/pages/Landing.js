d45ec25ff0a3eb381120fa671b3b024561d6580d
Valentina Antollini <antolliniv@hotmail.com>
2021-05-11 12:54:25 +0100

    star animatino

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ReactPlayer from "react-player";
+
 import { Link } from "react-router-dom";
 import backgroundVideo from "../images/bg/background.mp4";
 import retreatLogo from "../images/logo/retreat.png";
 import enterLogo from "../images/logo/enter.png";
-import ReactPlayer from "react-player";
+import starImage from "../images/general/star.png";
 
 export default function Landing() {
   return (
@@ -20,6 +22,9 @@ export default function Landing() {
       <Link to="/instructions" className="landing-link logo-container">
         <img src={retreatLogo} alt="" />
         <img src={enterLogo} alt="" />
+        <img src={starImage} alt="" id='star-right' className='star'/>
+        <img src={starImage} alt="" id='star-top' className='star'/>
+
       </Link>
     </div>
   );
