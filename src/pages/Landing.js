55da913182182171f29cdb90415b880be690a5e4
Valentina Antollini <antolliniv@hotmail.com>
2021-05-11 12:31:15 +0100

    landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import backgroundVideo from "../images/bg/background.mp4";
+import retreatLogo from "../images/logo/retreat.png";
+import enterLogo from "../images/logo/enter.png";
+import ReactPlayer from "react-player";
 
 export default function Landing() {
   return (
-    <div>
-      <h1> landing</h1>
+    <div class="background-landing">
+      <ReactPlayer
+        className=""
+        url={backgroundVideo}
+        width="100%"
+        height="100%"
+        controls={false}
+        loop={true}
+        playing={true}
+      />
+      <Link to="/instructions" className="landing-link logo-container">
+        <img src={retreatLogo} alt="" />
+        <img src={enterLogo} alt="" />
+      </Link>
     </div>
   );
 }
