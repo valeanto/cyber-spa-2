cc9a831a4a25b58031a4a1fc9ccab1dfbf3f8804
Valentina Antollini <antolliniv@hotmail.com>
2021-05-15 22:45:33 +0100

    instructions toggle

--- src/pages/Instructions.js
+++ src/pages/Instructions.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactPlayer from "react-player";
 import { Link } from "react-router-dom";
 import instructionsVideo from "../images/bg/background.mp4";
+import retreatLogo from "../images/logo/retreat.png";
 
 export default function Instructions() {
+  const [toggle, setToggle] = useState(false);
+  function showAbout() {
+    // document.getElementById("about_box").className = "active";
+  }
   return (
     <div className="instructions-landing">
       <ReactPlayer
@@ -15,6 +20,22 @@ export default function Instructions() {
         loop={true}
         playing={true}
       />
+      <div className="logo">
+        <img src={retreatLogo} alt="" />
+      </div>
+      <button className="about_button" onClick={() => setToggle(!toggle)}>
+        button
+        {toggle && (
+          <ul>
+            <li>Show me</li>
+            <li>Only when</li>
+            <li>Toggle === true</li>
+          </ul>
+        )}
+      </button>
+      <div className="about_box" id="about_box">
+        about box
+      </div>
       <Link to="/question-one" className="to-button-quiz float">
         Lets begin
       </Link>
