2c051d00b62d08457746637fde3d493119af3130
Valentina Antollini <antolliniv@hotmail.com>
2021-05-19 22:41:20 +0100

    quiz 2

--- src/pages/quiz/QuestionTwo.js
+++ src/pages/quiz/QuestionTwo.js
@@ -6,45 +6,49 @@ import air from "../../images/quiz/q2/air.png";
 import earth from "../../images/quiz/q2/earth.png";
 import water from "../../images/quiz/q2/water.png";
 import title from "../../images/quiz/q2/title.png";
-import waterVideo from "../../images/quiz/q2/water.mov";
-import mountainVideo from "../../images/quiz/q2/mountain.mov";
 import retreatLogo from "../../images/quiz/q2/retreat.png";
+import mountainGif from "../../images/quiz/q2/mountain-gif.gif";
+import waterGif from "../../images/quiz/q2/water.gif";
+import skyGif from "../../images/quiz/q2/sky.gif";
 
 export default function QuestionTwo() {
   return (
     <div className="questionContainer">
       <img src={retreatLogo} alt={title} className="retreatLogo" />
 
-      {/* <img src={}alt="" className="first-image" /> */}
+      <ReactPlayer
+        className="react-player quiz-video"
+        url={quizOneBackground}
+        width="100%"
+        height="100%"
+        controls={false}
+        loop={true}
+        playing={true}
+      />
       <div className="content questionTwo_container">
         <div className="q_title qTwo">
-          <img src={title} alt={title} className="q2_title" />
           <img src={title} alt={title} className="q2_title" />
         </div>
         <div className="q2_container">
           <div className="q2_item">
-            <img src={air} alt={air} className="first-image" />
+            <Link to="/three">
+              <img src={air} alt={air} className="first-image" />
+              <img src={skyGif} alt={earth} className="video_container" />
+            </Link>
           </div>
 
           <div className="q2_item ">
             <Link to="/three">
               <img src={earth} alt={earth} className="first-image" />
+              <img src={mountainGif} alt={earth} className="video_container" />
             </Link>
           </div>
 
           <div className="q2_item">
-            <img src={water} alt={water} className="first-image" />
-            <div className="video_container">
-              <ReactPlayer
-                className="video-image"
-                url={waterVideo}
-                width="100%"
-                height="100%"
-                controls={false}
-                loop={true}
-                playing={true}
-              />
-            </div>
+            <Link to="/three">
+              <img src={water} alt={water} className="first-image" />
+              <img src={waterGif} alt={earth} className="video_container" />
+            </Link>
           </div>
         </div>
       </div>
